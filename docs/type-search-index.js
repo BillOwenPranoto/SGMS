de25fa0cf6a29a6cb4de2a4e1762a8eadcfb915b
BillOwenPranoto <pranotobillowen@gmail.com>
2025-06-15 23:44:27 +0800

    Add generated Javadoc documentation

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"studentmanager","l":"Main"},{"p":"studentmanager","l":"Student"},{"p":"studentmanager","l":"StudentManager"},{"p":"studentmanager","l":"Subjects"}];updateSearchResults();
